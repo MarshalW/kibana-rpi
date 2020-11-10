5d9b9f376983270d62be9018377ef16cc32434c8
marshalw <marshal.wu@gmail.com>
2020-11-10 10:40:04 +0000

    update script

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Kibana docker file for RPi4
 
-当前版本，Kibana v7.9.3
+当前可用版本，Kibana v7.9.3，build命令：
+
+```properties
+cd v7.9.3
+docker build -t kibana-rpi .
+```
 
--- get-files.sh
+++ get-files.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+VERSION=$1
+
+rm -rf ./workbench/v$VERSION || true
+
+mkdir ./workbench/v$VERSION
+mkdir ./workbench/v$VERSION/bin
+mkdir ./workbench/v$VERSION/config
+
+curl https://raw.githubusercontent.com/elastic/dockerfiles/v$VERSION/kibana/Dockerfile \
+ --output ./workbench/v$VERSION/Dockerfile
+curl https://raw.githubusercontent.com/elastic/dockerfiles/v$VERSION/kibana/bin/kibana-docker \
+ --output ./workbench/v$VERSION/bin/kibana-docker
+curl https://raw.githubusercontent.com/elastic/dockerfiles/v$VERSION/kibana/config/kibana.yml \
+ --output ./workbench/v$VERSION/config/kibana.yml
